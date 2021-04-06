cf5b328fed7d6caa4898fedea1e2f59eeade4588
Peter Bayerle <peterbayerle2@gmail.com>
2021-04-06 14:34:35 -0700

    a fourth photo

--- src/pages/photo_test.js
+++ src/pages/photo_test.js
@@ -6,6 +6,8 @@ var PhotoTest = () => {
             <img className="rounded-circle profilePicture" src="profile0.png" alt="profile"></img> 
             <br></br>
             <img width={35} src="profile0.svg"></img>
+            <br></br>
+            <img className="rounded-circle profilePicture" src="https://raw.githubusercontent.com/peterbayerle/dpil_olympus/main/public/profile0.png" alt="profile"></img> 
         </>
     );
 };
